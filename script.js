07b7206f906e3e1cd6c4b5d18cdbef16798aff12
YohanmlMamaril <167493606+YohanmlMamaril@users.noreply.github.com>
2024-12-06 04:15:11 +0800

    Styled input bar
    -styled input bars
    -styled buttons

--- script.js
+++ script.js
@@ -71,9 +71,5 @@ function setAlarm() {
 }
 
 // Section Navigation
-function showSection(sectionId) {
-  document.querySelectorAll('main > section').forEach(section => {
-    section.classList.add('hidden');
-  });
-  document.getElementById(sectionId).classList.remove('hidden');
+
 }
